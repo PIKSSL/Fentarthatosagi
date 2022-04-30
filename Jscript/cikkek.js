a57c07d7e01735e6638fc1549cc2944ab270445b
PIKSSL <purpledog120@gmail.com>
2022-04-30 23:23:58 +0200

    Cikkek_cikk_kisablak

--- Jscript/cikkek.js
+++ Jscript/cikkek.js
@@ -4,28 +4,23 @@ window.addEventListener("load", init);
 
 
 function init(){
-fetch('../Tartalom/cikkek/cikktarolo.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data.cikkek);
-    var tomb=data.cikkek
-    tomb.forEach(element => {
-        document.getElementById("ctarolo").innerHTML = "<div id='cikk' class='1'><div class='keskep'><img src='"+element.kep+"' alt='#'></div><h3>"+element.focim+"</h3><p>Rövidleirásrövidleirásasdasdasdasdadasdasdasd</p></div>";;
-
-    });
-    
-
-  })
-    
+feltolt();
 }
 
 
 function feltolt(){
     let tartalom = "";
     let i = 0;
-    adatok.forEach(adat => {
-        tartalom+="<div id='cikk' class='"+i+"'><div class='keskep'><img src='"+adat.kep+"' alt='#'></div><h3>"+adat.focim+"</h3><p>Rövidleirásrövidleirásasdasdasdasdadasdasdasd</p></div>";
-        i++;
-    });
-    document.getElementById(tartalom).innerHTML=tartalom;
+    fetch('../Tartalom/cikkek/cikktarolo.json')
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data.cikkek);
+      var tomb=data.cikkek
+      console.log(tomb);
+      tomb.forEach(element => {
+          tartalom += "<div id='cikk' class='"+i+"'><div class='keskep'><img src='"+element.kep+"' alt='#'></div><h3>"+element.focim+"</h3><p>"+element.be1+"</p></div>";
+          i++;
+      });
+      document.getElementById("ctarolo").innerHTML=tartalom;
+    })
 }
